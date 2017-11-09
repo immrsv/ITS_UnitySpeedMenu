6ae71caf20901092c83a16ea9192a23aebe6f060
Immersive <immrsv@users.noreply.github.com>
2017-11-09 15:47:05 +1030

    Trialled different sprite layouts for button backing

---
+++ Assets/Scripts/AlphaHitTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaHitTest : MonoBehaviour {
+
+    public float AlphaThreshold = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+    void Update() {
+        GetComponent<UnityEngine.UI.Image>().alphaHitTestMinimumThreshold = AlphaThreshold;
+    }
+}
---
+++ Assets/Scripts/MouseLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseLogger : MonoBehaviour, 
+    IPointerClickHandler   
+    
+{
+    public void OnPointerClick(PointerEventData eventData) {
+        Debug.Log("Clicked on: " + gameObject.name);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
