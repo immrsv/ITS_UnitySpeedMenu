27273359bbeeea2ef72a7956ba7a30ec1d289c19
Immersive <immrsv@users.noreply.github.com>
2017-11-10 16:52:58 +1030

    Added Mouse tracking and Hide/Reveal

--- Assets/Scripts/PieMenu.cs
+++ Assets/Scripts/PieMenu.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PieMenu : MonoBehaviour, UnityEngine.EventSystems.I
+public class PieMenu : MonoBehaviour
 {
 
-    public GameObject OptionBack;
-    public List<GameObject> Options;
+    public GameObject Centre;
+    public GameObject[] Options;
+
+    public string ToggleButton = "Fire1";
+
+    public bool Visible = true;
+
+
 
     public void OnMove(AxisEventData eventData) {
         Debug.Log("OnMove(): " + eventData.moveVector);
@@ -22,9 +28,36 @@ public class PieMenu : MonoBehaviour, UnityEngine.EventSystems.I
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetButtonDown(ToggleButton))
+            Show();
+
+        if (Input.GetButtonUp(ToggleButton))
+            Hide();
+    }
+
+
+    void Show() {
+
+
+
+        Centre.SetActive(true);
+        for (var i = 0; i < Options.Length; i++) {
+            Options[i].SetActive(true);
+        }
+
+
+        transform.position = Input.mousePosition;
+    }
+
+
+    void Hide() {
+        Centre.SetActive(false);
+        for ( var i = 0; i < Options.Length; i++) {
+            Options[i].SetActive(false);
+        }
+    }
 }
